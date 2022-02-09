8847e1993f6f4342b7aa733a6ec7a3111e94f2fd
acbeni <beni.asaad.achraf@gmail.com>
2022-02-09 10:40:48 +0100

    Updates

---
+++ _app/pages/__layout.svelte-41d969bd.js
@@ -0,0 +1,13 @@
+import{S as _,i as f,s as m,e as g,t as y,F as v,c as p,a as h,d as i,h as b,G as L,b as j,H as l,I as u,J as G,w as M,k as P,x as S,m as T,y as q,g as C,K as H,L as k,M as A,q as $,o as w,B as E,v as I}from"../chunks/vendor-bbbb77bf.js";import{c as x}from"../chunks/AuthService-06c3ca4a.js";function D(r){let e,o,s,n;return{c(){e=g("script"),s=g("script"),n=y(`window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-5PT8M92H35');`),this.h()},l(a){const t=v('[data-svelte="svelte-ude3sm"]',document.head);e=p(t,"SCRIPT",{src:!0});var c=h(e);c.forEach(i),s=p(t,"SCRIPT",{});var d=h(s);n=b(d,`window.dataLayer = window.dataLayer || [];
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-5PT8M92H35');`),d.forEach(i),t.forEach(i),this.h()},h(){e.async=!0,L(e.src,o="https://www.googletagmanager.com/gtag/js?id=G-5PT8M92H35")||j(e,"src",o)},m(a,t){l(document.head,e),l(document.head,s),l(s,n)},p:u,i:u,o:u,d(a){i(e),i(s)}}}class R extends _{constructor(e){super();f(this,e,null,D,m,{})}}function B(r){let e,o,s;e=new R({});const n=r[1].default,a=G(n,r,r[0],null);return{c(){M(e.$$.fragment),o=P(),a&&a.c()},l(t){S(e.$$.fragment,t),o=T(t),a&&a.l(t)},m(t,c){q(e,t,c),C(t,o,c),a&&a.m(t,c),s=!0},p(t,[c]){a&&a.p&&(!s||c&1)&&H(a,n,t,t[0],s?A(n,t[0],c,null):k(t[0]),null)},i(t){s||($(e.$$.fragment,t),$(a,t),s=!0)},o(t){w(e.$$.fragment,t),w(a,t),s=!1},d(t){E(e,t),t&&i(o),a&&a.d(t)}}}function F(r,e,o){let{$$slots:s={},$$scope:n}=e;return I(()=>{x()}),r.$$set=a=>{"$$scope"in a&&o(0,n=a.$$scope)},[n,s]}class z extends _{constructor(e){super();f(this,e,F,B,m,{})}}export{z as default};
